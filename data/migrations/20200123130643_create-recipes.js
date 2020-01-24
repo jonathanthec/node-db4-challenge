bec199d049f7883d26bf53299129788e2ff4ebfb
JonathanChen <jonny29009@yahoo.com>
2020-01-23 17:36:21 -0800

    create database table schemes

--- data/migrations/20200123130643_create-recipes.js
+++ data/migrations/20200123130643_create-recipes.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema
+        .createTable("recipe", tbl => {
+            tbl.increments();
+            tbl.string("recipe_title", 128)
+                .notNullable()
+                .index();
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+        .dropTableIfExists("recipe");
 };
